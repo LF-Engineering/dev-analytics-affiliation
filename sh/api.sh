b1e32c801cfd7c9080850ff8481d69a20c1ee62c
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-03-19 13:30:54 +0000

    Figured out ES query that is needed to get results
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sh/api.sh
+++ sh/api.sh
@@ -4,7 +4,10 @@ export SH_DB_ENDPOINT='sortinghat:pwd@tcp(localhost:13306)/sortinghat?charset=ut
 export AUTH0_DOMAIN=`cat secret/auth0.domain`
 export AUTH0_CLIENT_ID=`cat secret/auth0.client_id`
 export AUTH0_USERNAME_CLAIM=`cat secret/auth0.username_claim`
-export ELASTIC_URL='http://127.0.0.1:19200'
+if [ -z "$ELASTIC_URL" ]
+then
+  export ELASTIC_URL='http://127.0.0.1:19200'
+fi
 export ELASTIC_USERNAME=''
 export ELASTIC_PASSWORD=''
 if [ -z "$ONLYRUN" ]
---
+++ sh/curl_es_unaffiliated.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+  echo "$0: please provide project slug as a first arg"
+  exit 1
+fi
+if [ -z "$ES_URL" ]
+then
+  export ES_URL="http://127.0.0.1:19200"
+fi
+idx="sds-${1//\//-}"
+curl -H 'Content-Type: application/json' "${ES_URL}/${idx}-*,-${idx}-*-raw/_search" -d'{"size":0,"aggs":{"unaffiliated":{"terms":{"field":"author_org_name","missing":"null","size":10}}}}' 2>/dev/null | jq
+curl -H 'Content-Type: application/json' "${ES_URL}/${idx}-*,-${idx}-*-raw/_search" -d'{"size":0, "aggs":{"unaffiliated":{"filter":{"terms":{"author_org_name":["Unknown", "NotFound", "", "-", "?"]}},"aggs":{"unaffiliated":{"terms":{"field":"author_name","missing":"","size": 10}}}}}}' 2>/dev/null | jq
