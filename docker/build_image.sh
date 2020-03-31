3432374d270fa584ee519331ba30cb0417cf5a71
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-03-31 12:33:41 +0000

    Remove comment
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# DOCKER_USER=lukaszgryglicki SKIP_TEST=1 SKIP_PROD=1 SKIP_FULL=1 SKIP_MIN=1 SKIP_GRAFANA=1 SKIP_TESTS=1 SKIP_PATRONI=1 SKIP_STATIC=1 SKIP_REPORTS=1 SKIP_API=1 SKIP_PUSH=1 ./images/build_images.sh
 if [ -z "${DOCKER_USER}" ]
 then
   echo "$0: you need to set docker user via DOCKER_USER=username"
