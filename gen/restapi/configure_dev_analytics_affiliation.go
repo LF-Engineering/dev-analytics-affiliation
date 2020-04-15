a8dbf1d8e93ded5601731882c09844a71dce73cc
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-04-15 17:23:26 +0000

    Use AllowAll CORS handler
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- gen/restapi/configure_dev_analytics_affiliation.go
+++ gen/restapi/configure_dev_analytics_affiliation.go
@@ -235,24 +235,26 @@ func setupMiddlewares(handler http.Handler) http.Handler {
 // So this is a good place to plug in a panic handling middleware, logging and metrics
 func setupGlobalMiddleware(handler http.Handler) http.Handler {
 	//handleCORS := cors.Default().Handler
-	//handleCORS := cors.AllowAll().Handler
-	handleCORS := cors.New(cors.Options{
-		AllowedOrigins: []string{
-			"https://test.lfanalytics.io",
-			"https://lfanalytics.io",
-			//"https://*.lfanalytics.io",
-		},
-		AllowedMethods: []string{
-			http.MethodGet,
-			http.MethodPost,
-			http.MethodPut,
-			http.MethodDelete,
-			http.MethodPatch,
-			http.MethodOptions,
-			http.MethodHead,
-		},
-		AllowCredentials: true,
-		Debug:            true,
-	}).Handler
+	handleCORS := cors.AllowAll().Handler
+	/*
+		handleCORS := cors.New(cors.Options{
+			AllowedOrigins: []string{
+				"https://test.lfanalytics.io",
+				"https://lfanalytics.io",
+				//"https://*.lfanalytics.io",
+			},
+			AllowedMethods: []string{
+				http.MethodGet,
+				http.MethodPost,
+				http.MethodPut,
+				http.MethodDelete,
+				http.MethodPatch,
+				http.MethodOptions,
+				http.MethodHead,
+			},
+			AllowCredentials: true,
+			Debug:            true,
+		}).Handler
+	*/
 	return handleCORS(handler)
 }
