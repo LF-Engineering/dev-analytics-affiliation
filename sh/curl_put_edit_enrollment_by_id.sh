ebb54d175a369c0001d0a4ed046e0f92d8ebe9e5
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-06-24 07:29:54 +0000

    Added EditEnrollmentByID API
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ sh/curl_put_edit_enrollment_by_id.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+. ./sh/shared.sh
+if [ -z "$2" ]
+then
+  echo "$0: please specify enrollment ID as a 2nd arg"
+  exit 2
+fi
+enrollment_id=$(rawurlencode "${2}")
+extra=''
+
+for prop in merge new_start new_end new_is_project_specific
+do
+  if [ ! -z "${!prop}" ]
+  then
+    encoded=$(rawurlencode "${!prop}")
+    if [ -z "$extra" ]
+    then
+      extra="?$prop=${encoded}"
+    else
+      extra="${extra}&$prop=${encoded}"
+    fi
+  fi
+done
+
+if [ ! -z "$DEBUG" ]
+then
+  echo curl -i -s -H "Origin: ${ORIGIN}" -H "Authorization: Bearer ${JWT_TOKEN}" -XPUT "${API_URL}/v1/affiliation/${project}/edit_enrollment_by_id/${enrollment_id}${extra}"
+  curl -i -s -H "Origin: ${ORIGIN}" -H "Authorization: Bearer ${JWT_TOKEN}" -XPUT "${API_URL}/v1/affiliation/${project}/edit_enrollment_by_id/${enrollment_id}${extra}"
+else
+  curl -s -H "Origin: ${ORIGIN}" -H "Authorization: Bearer ${JWT_TOKEN}" -XPUT "${API_URL}/v1/affiliation/${project}/edit_enrollment_by_id/${enrollment_id}${extra}"
+fi
