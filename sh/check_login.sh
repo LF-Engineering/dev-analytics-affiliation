9ec8b390458bbd93a6cdc60394707afd603320fa
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-08-27 08:06:44 +0000

    Check login script finished
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sh/check_login.sh
+++ sh/check_login.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
+# Example:
+# PSQL='sudo -u postgres psql db' MYSQL='mysql -hhost -uuser -ppass db' ./sh/check_login.sh 'lukasz Gryglicki'
 if [ -z "$MYSQL" ]
 then
   echo "Please specify full mysql connect command, something like MYSQL='mysql -hdburl -uuser -ppassword dbname'"
   exit 1
+fi
+if [ -z "$PSQL" ]
+then
+  echo "Please specify full postgresql connect command, something like PSQL='sudo -u postgres psql dbname'"
+  exit 2
 fi
 if [ -z "${JSON}" ]
 then
   JSON="${HOME}/dev/go/src/github.com/cncf/devstats/github_users.json"
 fi
 MYSQL="${MYSQL} -NBAe "
+PSQL="${PSQL} -F$'\t' -tAc "
 for name in "$@"
 do
   echo "${name}:"
+  echo "SortingHat:"
   cmd="$MYSQL \"select uuid from profiles where name = '${name}' union select uuid from identities where name = '${name}'\""
   uuids=$(eval "${cmd}")
   if [ -z "${uuids}" ]
@@ -22,11 +31,12 @@ do
   i="1"
   declare -A logins=()
   declare -A emails=()
+  declare -A eemails=()
   for uuid in ${uuids}
   do
     echo "#${i} uuid: ${uuid}"
     echo "Identities:"
-    cmd="$MYSQL \"select source, username, regexp_replace(email, '([^\\s@]+)@([^\\s@]+)', '\\\\\\\\1\\!\\\\\\\\2') from identities where uuid = '${uuid}' order by source\""
+    cmd="$MYSQL \"select source, username, email, regexp_replace(email, '([^\\s@]+)@([^\\s@]+)', '\\\\\\\\1\\!\\\\\\\\2') from identities where uuid = '${uuid}' order by source\""
     data=$(eval "${cmd}")
     if [ -z "${data}" ]
     then
@@ -42,20 +52,22 @@ do
       src="${ary[${j}]}"
       login="${ary[((j+1))]}"
       email="${ary[((j+2))]}"
+      eemail="${ary[((j+3))]}"
       if [ -z "${src}" ]
       then
         break
       fi
       echo -e "${src}\t${login}\t${email}"
-      if [ ! "${login}" = "NULL" ]
+      if ( [ "${src}" = "github" ] && [ ! "${login}" = "NULL" ] )
       then
         logins["${login}"]="1"
       fi
       if [ ! "${email}" = "NULL" ]
       then
         emails["${email}"]="1"
+        eemails["${eemail}"]="1"
       fi
-      ((j=j+3))
+      ((j=j+4))
     done
     echo "Enrollments:"
     cmd="$MYSQL \"select e.project_slug, date(e.start), date(e.end), o.name, e.role from enrollments e, organizations o where e.organization_id = o.id and e.uuid = '${uuid}' order by e.project_slug, e.start\""
@@ -70,6 +82,8 @@ do
   done
   i="1"
   conds=""
+  lcond=""
+  econd=""
   for login in "${!logins[@]}"
   do
     cond=".login==\"${login}\""
@@ -79,10 +93,25 @@ do
     else
       conds="${conds} or ${cond}"
     fi
+    if [ -z "${lcond}" ]
+    then
+      lcond="'${login}'"
+    else
+      lcond="${lcond},'${login}'"
+    fi
   done
   for email in "${!emails[@]}"
   do
-    cond=".email==\"${email}\""
+    if [ -z "${econd}" ]
+    then
+      econd="'${email}'"
+    else
+      econd="${econd},'${email}'"
+    fi
+  done
+  for eemail in "${!eemails[@]}"
+  do
+    cond=".email==\"${eemail}\""
     if [ -z "${conds}" ]
     then
       conds="${cond}"
@@ -90,7 +119,46 @@ do
       conds="${conds} or ${cond}"
     fi
   done
-  echo "Cond: ${conds}"
+  echo "CNCF JSON:"
   js=`jq -r ".[] | select(${conds}) | .login + \"/\" + .email + \": \" + (.affiliation // \"-\")" "${JSON}"`
+  if [ -z "${js}" ]
+  then
+    echo "Nothing found for: ${conds}"
+  else
+    echo "${js}"
+  fi
   echo 
+  pcond=''
+  if [ ! -z "${lcond}" ]
+  then
+    pcond="select id from gha_actors where login in (${lcond})"
+  fi
+  if [ ! -z "${econd}" ]
+  then
+    if [ -z "${pcond}" ]
+    then
+      pcond="select actor_id from gha_actors_emails where email in (${econd})"
+    else
+      pcond="${pcond} union select actor_id from gha_actors_emails where email in (${econd})"
+    fi
+  fi
+  echo "DevStats DB:"
+  if [ -z "${pcond}" ]
+  then
+    echo "No email or login to search data found"
+    echo
+    continue
+  fi
+  echo 'Affiliations:'
+  cmd="$PSQL \"select date(dt_from), date(dt_to), company_name, source from gha_actors_affiliations where actor_id in (${pcond})\""
+  data=$(eval "${cmd}")
+  echo "${data}"
+  echo 'Commits:'
+  cmd="$PSQL \"select count(distinct sha) as cnt, date(min(dup_created_at)), date(max(dup_created_at)) from gha_commits where committer_id in (${pcond}) or author_id in (${pcond}) order by cnt desc\""
+  data=$(eval "${cmd}")
+  echo "${data}"
+  echo 'Events:'
+  cmd="$PSQL \"select type, count(*) as cnt, date(min(created_at)), date(max(created_at)) from gha_events where actor_id in (${pcond}) group by type order by cnt desc\""
+  data=$(eval "${cmd}")
+  echo "${data}"
 done
