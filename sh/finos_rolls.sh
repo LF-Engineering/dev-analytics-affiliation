f59055dd8a7a5aca372f2a067b5d285bb2e115b2
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-07-14 07:50:33 +0000

    Update FINOS rolls scripts
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sh/finos_rolls.sh
+++ sh/finos_rolls.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # James McLeod
-./sh/update_finos_rolls.sh prod d89e39413399a38f1780456e37dfb4ee16810e13 'Individual - No Account;;2013-11-01' 'Publicis Sapient;2013-11-01;2017-06-01' 'Lloyds Banking;2017-06-01;2019-08-31' 'Finos;2019-08-31;2020-04-08' 'The Linux Foundation;2020-04-08;'
+./sh/update_finos_rolls.sh prod d89e39413399a38f1780456e37dfb4ee16810e13 'Individual - No Account;;2013-11-01' 'Publicis Sapient;2013-11-01;2017-06-01' 'Lloyds Bank plc;2017-06-01;2019-08-31' 'Finos;2019-08-31;2020-04-08' 'The Linux Foundation;2020-04-08;'
 # Maurizio Pillitu
 ./sh/update_finos_rolls.sh prod 7d25f805bcc1886acd05a50efdacd1c95dff8912 'Individual - No Account;;2007-09-01' 'Sourcesense;2007-09-01;2012-01-31' 'Individual - No Account;2012-01-31;2012-04-01' 'Alfresco;2012-04-01;2016-04-30' 'Finos;2016-04-30;2020-04-08' 'The Linux Foundation;2020-04-08;'
 # Gabriele Columbro
