9a0d7c46ac247f8b58d7c7e9aebcf5e39b2952cc
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-07-21 05:49:21 +0000

    Update FINOS rolls script
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sh/finos_rolls.sh
+++ sh/finos_rolls.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# CHECK=1 - only check data
 # James McLeod
 ./sh/update_finos_rolls.sh prod d89e39413399a38f1780456e37dfb4ee16810e13 'Individual - No Account;;2013-11-01' 'Publicis Sapient;2013-11-01;2017-06-01' 'Lloyds Bank plc;2017-06-01;2019-08-31' 'Finos;2019-08-31;2020-04-08' 'The Linux Foundation;2020-04-08;'
 # Maurizio Pillitu
