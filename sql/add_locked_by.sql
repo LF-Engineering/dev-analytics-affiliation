49855f30071c56f28b0263e160c013a373efafe0
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-01-14 12:05:18 +0000

    Missing indices
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- sql/add_locked_by.sql
+++ sql/add_locked_by.sql
@@ -3,3 +3,8 @@ alter table enrollments add locked_by varchar(128);
 alter table identities add locked_by varchar(128);
 alter table profiles add locked_by varchar(128);
 alter table uidentities add locked_by varchar(128);
+-- Indices
+create index enrollments_locked_by_idx on enrollments(locked_by);
+create index identities_locked_by_idx on identities(locked_by);
+create index profiles_locked_by_idx on profiles(locked_by);
+create index uidentities_locked_by_idx on uidentities(locked_by);
